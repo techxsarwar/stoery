508ed65208f8a81940826066b051ee5520b6a9f9
Sarwar Altaf <contact@sarwaraltaf.in>
2026-04-19 17:32:40 +0530

    Fix: Final type-check bypass using empty string for password

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -45,7 +45,7 @@ export async function signUp(formData: FormData) {
           id: data.user.id, // Use the Supabase UID as our ID
           name,
           email,
-          password: null, // Manually passing null tells Prisma "I meant to leave this blank"
+          password: "", // Change null to an empty string
         },
       });
     } catch (e) {
