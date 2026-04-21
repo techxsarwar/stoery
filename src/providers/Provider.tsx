57876858ff03662c80d429542dbea26afbdcaf58
Sarwar Altaf <contact@sarwaraltaf.in>
2026-04-21 15:24:35 +0530

    migrate back to supabase

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -1,5 +1,3 @@
-import { NextAuthProvider } from "./NextAuthProvider";
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextAuthProvider>{children}</NextAuthProvider>;
+  return <>{children}</>;
 }
